39dadb49f870da2ce18229ad141162d5926b1faa
Dennis Patzer <d.patzer@heimrich-hannot.de>
2018-11-06 15:16:51 +0100

    fixed be nav

--- src/Resources/contao/config/config.php
+++ src/Resources/contao/config/config.php
@@ -6,11 +6,16 @@
 $GLOBALS['TL_MODELS']['tl_calendar_sub_events'] = 'HeimrichHannot\EventsBundle\Model\CalendarSubEventsModel';
 
 /**
- *
+ * Backend modules
  */
-$GLOBALS['BE_MOD']['content']['calendar_subevents'] = array
-(
-    'tables'      => array('tl_calendar_sub_events', 'tl_content'),
-    'table'       => array('TableWizard', 'importTable'),
-    'list'        => array('ListWizard', 'importList')
-);
+$GLOBALS['BE_MOD']['content']['calendar_subevents'] = [
+    'tables' => ['tl_calendar_sub_events', 'tl_content']
+];
+
+/**
+ * Assets
+ */
+if (System::getContainer()->get('huh.utils.container')->isBackend())
+{
+    $GLOBALS['TL_CSS']['events-bundle'] = 'bundles/heimrichhannotcontaoevents/css/contao-events-bundle.min.css|static';
+}
